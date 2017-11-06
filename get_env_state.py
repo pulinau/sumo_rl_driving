74d15e13240e4783a361fefeec07547f5ef94edd
Ken Li <ken@wise13.eng.uwaterloo.ca>
2017-11-05 21:28:36 -0500

    add observation space implementation(not complete)

--- get_env_state.py
+++ get_env_state.py
@@ -19,20 +19,20 @@ def get_vehicle_state():
   get the current state of all the vehicles.
   Returns:
     veh_state_dict:
-    {"type": "vehicle", "location": ...}
+    {"type": "vehicle", "position": ...}
   """
   veh_id_list = traci.vehicle.getIDList()
   veh_state_dict = {}
   for veh_id in veh_id_list:
     veh_state_dict[veh_id] = {}
     veh_state_dict[veh_id]["type"] = "vehicle"
-    veh_state_dict[veh_id]["location"] = traci.vehicle.getPosition(veh_id)
+    veh_state_dict[veh_id]["position"] = traci.vehicle.getPosition(veh_id)
     veh_state_dict[veh_id]["angle"] = traci.vehicle.getAngle(veh_id) #in degree. North is zero, clockwise
-    veh_state_dict[veh_id]["velocity"] = traci.vehicle.getSpeed(veh_id)
+    veh_state_dict[veh_id]["speed"] = traci.vehicle.getSpeed(veh_id)
     veh_state_dict[veh_id]["dimension"]  = (traci.vehicle.getLength(veh_id),traci.vehicle.getWidth(veh_id))
     veh_state_dict[veh_id]["edge"] = traci.vehicle.getRoadID(veh_id)
     veh_state_dict[veh_id]["lane"] = traci.vehicle.getLaneID(veh_id)
-    veh_state_dict[veh_id]["lane_length"] = traci.lane.getLength(veh_state_dict[veh_id]["lane"])
+    veh_state_dict[veh_id]["lane_length"] = traci.lane.getLength(veh_state_dict[veh_id]["lane"]) #position in the lane
     veh_state_dict[veh_id]["lane_position"] = traci.vehicle.getLanePosition(veh_id)
     veh_state_dict[veh_id]["route"] = traci.route.getEdges(traci.vehicle.getRouteID(veh_id))
   return veh_state_dict
@@ -81,5 +81,6 @@ def get_lanelet_graph(sumo_net_xml_file):
       connection["dst_lanelet_id"] = sumo_edge.getLanes()[i].getID()
       connection["connection_type"] = "right"
       connection_list += [connection.copy()]
-  # "left" and "right" connection for opposite direction lane is not added
+  #"left" and "right" connection for opposite direction lane is not added
   return (lanelet_id_list, connection_list)
+ 
--- sumo_gym.py
+++ sumo_gym.py
@@ -1,4 +1,5 @@
 #!python3
+__author__ = "Changjian Li"
 
 SUMO_TOOLS_DIR = "/home/ken/project/sumo-0.30.0/tools"
 SUMO_BIN = "/home/ken/project/sumo-0.30.0/bin/sumo"
@@ -12,19 +13,22 @@ SUMO_CMD = [SUMO_BIN, "-c", SUMO_CONFIG, "--time-to-teleport", "-1"]
 MAX_VEHICLE_SPEED = 200
 MAX_LANE_PRIORITY = 2
 NUM_VEHICLE_CONSIDERED = 16
+OBSERVATION_RADIUS = 600
 
 import gym
 from gym import spaces
 import numpy as np
 import sys
+import time
 from math import pi
 
 try:
   sys.path.append(SUMO_TOOLS_DIR)
 except ImportError:
   print("Please modify SUMO_TOOLS_DIR to the location of sumo tools")
-import time
 import traci
+
+from get_env_state import get_vehicle_state, get_lanelet_graph
 
 Class InvalidAction(Exception):
   pass
@@ -44,10 +48,13 @@ Class SumoEnv(gym.Env):
   observation space is a spaces.Dict object, whose key is mostly self explanatory:
     1) "ego_speed"
     2) "ego_dist_to_end_of_lane"
-    3) "exists_vehicle": spaces.MultiDiscrete object, each of whose element indicates whether a vehicle exists[1] or not[0]
-    4) "relative_position": space.Box object of size (NUM_VEHICLE_CONSIDERED, 2), each the position of the vehicle relative to the ego vehicle 
-    5) "dist_to_end_of_lane"
-    6) "lane_relation": the relationship between the vehicle lane and the ego lane. Possible reltionships are:
+    3) "exists_vehicle": spaces.MultiDiscrete object, each of whose element indicates whether a vehicle exists[1] or not[0], we only
+                         consider vehicles within the range 
+    4) "speed": absolute speed of each vehicle
+    5) "relative_position": space.Box object of size (NUM_VEHICLE_CONSIDERED, 2), each the position of the vehicle relative to the ego vehicle 
+    6) "relative_heading": in radius, anticlockwise
+    7) "dist_to_end_of_lane"
+    8) "lane_relation": the relationship between the vehicle lane and the ego lane. Possible reltionships are:
          PEER[0]: share the same NEXT lane with the ego lane
          CONFLICT[1]: share the same intersection with the ego lane, and its route conflict that of the ego route
          CONFLICT_INTERSECTION[2]: the lane is inside the intersection ego is approaching, and conflicts with ego route
@@ -56,7 +63,8 @@ Class SumoEnv(gym.Env):
          LEFT[5]: to the left of ego lane
          RIGHT[6]: to the right of ego lane
          IRRELEVANT[7]: the lane is irrelevant to ego vehicle, e.g. opposite lane, in or approaching other intersections, 
-                      or in the same intersection but with no conflict with ego route 
+                      or in the same intersection but with no conflict with ego route
+         since a lane can have several of the above relationship with the ego lane at the same time, 8 of them is needed per vehicle
   If we feed the observation space to a neural network, the weights connecting to the same key (of the observation space dictionary) should be identical
   If the value of "exists_vehicle" is zero for index i, then the value of all other keys at index i are set to zero
   The weight connection to key "exists_vehicle" should cancel out the effect of other keys, 
@@ -74,10 +82,11 @@ Class SumoEnv(gym.Env):
                                          "ego_dist_to_end_of_lane": spaces.Box(0, float("inf")),
                                          "ego_in_intersection": spaces.Discrete(2)
                                          "exists_vehicle": spaces.MultiDiscrete([[0,1]] * NUM_VEHICLE_CONSIDERED)
+                                         "speed": spaces.Box(0, MAX_VEHICLE_SPEED, (NUM_VEHICLE_CONSIDERED,))
                                          "relative_position": spaces.Box(float("-inf"), float("inf"), (NUM_VEHICLE_CONSIDERED, 2)),
                                          "relative_heading": spaces.Box(-pi, pi, (NUM_VEHICLE_CONSIDERED,)),
                                          "dist_to_end_of_lane": spaces.Box(0, float("inf"), (NUM_VEHICLE_CONSIDERED,)),
-                                         "lane_relation": spaces.MultiDiscrete([[0,7]] * NUM_VEHICLE_CONSIDERED)
+                                         "lane_relation": spaces.MultiDiscrete([[0,7]] * 8 * NUM_VEHICLE_CONSIDERED)
                                          })
     #self.obsevation_space = spaces.Dict({
                                          ##TO DO: define MultiBox
@@ -117,3 +126,45 @@ Class SumoEnv(gym.Env):
     pass
   def _close(self):
     traci.colse()
+
+def get_observation():
+  veh_state_dict = get_vehicle_state()
+  (lane_id_list, connection_list) = get_lanelet_graph()
+  obs_dict = {}
+  obs_dict["ego_speed"] = veh_state_dict["ego"]["speed"]
+  obs_dict["ego_dist_to_end_of_lane"] = veh_state_dict["ego"]["lane_length"] - veh_state_dict["ego"]["lane_position"]
+  #if the ego lane is not in the lanelet list returned by sumolib, then it's an intersection
+  #sumolib only return normal (non-internal) edges
+  if veh_state_dict["ego_speed"]["lane"] not in lane_id_list:
+    obs_dict["ego_in_intersection"] = 1
+  else:
+    obs_dict["ego_in_intersection"] = 0
+  
+  #vehicles inside the region of insterest (within the OBSERVATION_RADIUS) except for ego
+  def in_ROI(ego_position, veh_position):
+    if veh_position[0] > ego_position[0]-OBSERVATION_RADIUS and
+       veh_position[1] > ego_position[1]-OBSERVATION_RADIUS and
+       veh_position[0] < ego_position[0]+OBSERVATION_RADIUS and
+       veh_position[1] < ego_position[1]+OBSERVATION_RADIUS:
+      return True
+    return False
+  veh_state_dict_ROI = {k: v for k, v in veh_state_dict.items()
+                        if k!="ego" and in_ROI(veh_state_dict["ego"]["position"], v["position"][0])
+                        }
+  
+  #now deal with the relavant vehicles
+  obs_dict["exists_vehicle"] = []
+  obs_dict["speed"] = []
+  obs_dict["relative_position"] = []
+  obs_dict["relative_heading"] = []
+  obs_dict["dist_to_end_of_lane"] = []
+  obs_dict["lane_relation"] = []
+  for veh_id, state_dict in veh_state_dict_ROI.items():
+    obs_dict["exists_vehicle"] += [1]
+    obs_dict["speed"] += [state_dict["speed"]]
+    obs_dict["relative_position"] += [state_dict["position"] - veh_state_dict["ego"]["position"]]
+    obs_dict["relative_heading"] += [-(state_dict["angle"] - veh_state_dict["ego"]["angle"])/180 * pi]
+    obs_dict["dist_to_end_of_lane"] += state_dict[veh_id]["lane_length"] - state_dict[veh_id]["lane_position"]
+    #check if the each of the possible relationship holds for the vehicle lane 
+    obs_dict["lane_relation"] = 
+  pass
