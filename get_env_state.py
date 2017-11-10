9643712bf7898bd55c633f54bc5a438012723f88
Ken Li <ken@wise13.eng.uwaterloo.ca>
2017-11-10 00:15:20 -0500

    redesign lanlet graph

--- get_env_state.py
+++ get_env_state.py
@@ -30,57 +30,89 @@ def get_vehicle_state():
     veh_state_dict[veh_id]["angle"] = traci.vehicle.getAngle(veh_id) #in degree. North is zero, clockwise
     veh_state_dict[veh_id]["speed"] = traci.vehicle.getSpeed(veh_id)
     veh_state_dict[veh_id]["dimension"]  = (traci.vehicle.getLength(veh_id),traci.vehicle.getWidth(veh_id))
-    veh_state_dict[veh_id]["edge"] = traci.vehicle.getRoadID(veh_id)
-    veh_state_dict[veh_id]["lane"] = traci.vehicle.getLaneID(veh_id)
+    veh_state_dict[veh_id]["edge_id"] = traci.vehicle.getRoadID(veh_id)
+    veh_state_dict[veh_id]["lane_id"] = traci.vehicle.getLaneID(veh_id)
     veh_state_dict[veh_id]["lane_length"] = traci.lane.getLength(veh_state_dict[veh_id]["lane"]) #position in the lane
     veh_state_dict[veh_id]["lane_position"] = traci.vehicle.getLanePosition(veh_id)
     veh_state_dict[veh_id]["route"] = traci.route.getEdges(traci.vehicle.getRouteID(veh_id))
   return veh_state_dict
 
-def get_lanelet_graph(sumo_net_xml_file):
+class lanelet_graph:
   """
-  Create the lanelet graph from net.xml file using sumolib.
-  lanelet ids are the vertices of the graph and lanelet connections are the edges
-  Args:
-    file path of net.xml file
-  Returns:
-    (lanelet_id_list, connection_list)
-    lanelet_id_list:
-      [lanelet_id_0, lanelet_id_1...]
-    connection_list:
-      [{"src_lanelet_id": src_lanelet_id, "dst_lanelet_id": dst_lanelet_id, "connection_type": "next"}, ...]
+  lanelet graph class 
   """
-  sumo_net = sumolib.net.readNet(sumo_net_xml_file)
-  lanelet_id_list = []
-  connection_list = []
-  sumo_edges = sumo_net.getEdges()
+  def __init__(self):
+    """
+    Create the lanelet graph from net.xml file using sumolib.
+    the lanelet graph is represented as a dictionary of the following structure:
+      self.lanelet_dict:
+        {"lanelet_id0": {"waypoint": [p0, p1...], 
+                        "from_node_id": from_node_id, 
+                        "to_node_id": to_node_id, 
+                        "edge_id": edge_id,
+                        "next_lane_id_list": next_lane_id_list,
+                        "previous_lane_id_list": previous_lane_id_list,
+                        "left_lane_id": left_lane_id,
+                        "right_lane_id": right_lane_id,
+                         }, ...
+         }
+    Args:
+      file path of net.xml file
+    """
+    net = sumolib.net.readNet(sumo_net_xml_file)
+    self.lanelet_dict = {}
+    edges = net.getEdges()
+
+    for edge in edges:
+      # add "next" and "previous" connection
+      # since there's no direction sumolib API to get previous lane, we need to do some tricks here
+      for lane in edge.getLanes():
+        lane_id = lane.getID()
+        self.lanelet_dict[lane_id] = {}
+        self.lanelet_dict[lane_id]["previous_lane_id_list"] = []
+      for lane_index, lane in enumerate(edge.getLanes()):
+        lane_id = lane.getID()
+        self.lanelet_dict[lane_id]["waypoint"] = traci.lane.getShape(lane_id)
+        self.lanelet_dict[lane_id]["from_node_id"] = edge.getFromNode().getID()
+        self.lanelet_dict[lane_id]["to_node_id"] = edge.getToNode().getID()
+        self.lanelet_dict[lane_id]["edge_id"] = edge.getID()
+        self.lanelet_dict[lane_id]["next_lane_id_list"] = [conn.getToLane().getID() for conn in lane.getOutgoing()] 
+        for next_lane_id in self.lanelet_dict[lane_id]["next_lane_id_list"]:
+          self.lanelet_dict[next_lane_id]["previous_lane_id_list"] += [lane_id]
+        if lane_index == len(edge.getLanes()):
+          self.lanelet_dict[lane_id]["left_lane_id"] = None
+        else:
+          self.lanelet_dict[lane_id]["left_lane_id"] = edge.getLanes()[lane_index+1].getID()
+        if lane_index == 0:
+          self.lanelet_dict[lane_id]["right_lane_id"] = None
+        else
+          self.lanelet_dict[lane_id]["right_lane_id"] = edge.getLanes()[lane_index-1].getID()
+      #"left" and "right" connection for opposite direction lane is not added
+    pass
   
-  for sumo_edge in sumo_edges:
-    # add "next" and "previous" connection
-    for sumo_lane in sumo_edge.getLanes():
-      lanelet_id_list += [sumo_lane.getID()]
-      for sumo_connection in sumo_lane.getOutgoing():
-        connection = {}
-        connection["src_lanelet_id"] = sumo_lane.getID()
-        connection["dst_lanelet_id"] = sumo_connection.getToLane().getID()
-        connection["connection_type"] = "next"
-        connection_list += [connection.copy()]
-        # add the reverse connection
-        connection["src_lanelet_id"] = connection["dst_lanelet_id"]
-        connection["dst_lanelet_id"] = sumo_lane.getID()
-        connection["connection_type"] = "previous"
-        connection_list += [connection.copy()]
-    # add "left" and "right" connection     
-    for i in range (0, len(sumo_edge.getLanes()) - 1):
-      connection = {}
-      connection["src_lanelet_id"] = sumo_edge.getLanes()[i].getID()
-      connection["dst_lanelet_id"] = sumo_edge.getLanes()[i+1].getID()
-      connection["connection_type"] = "left"
-      connection_list += [connection.copy()]
-      connection["src_lanelet_id"] = sumo_edge.getLanes()[i+1].getID()
-      connection["dst_lanelet_id"] = sumo_edge.getLanes()[i].getID()
-      connection["connection_type"] = "right"
-      connection_list += [connection.copy()]
-  #"left" and "right" connection for opposite direction lane is not added
-  return (lanelet_id_list, connection_list)
- 
+  def get_next_lane_id_list(self, lane_id):
+    return self.lanelet_dict[lane_id]["next_lane_id_list"]
+  
+  def get_previous_lane_id_list(self, lane_id):
+    return self.lanelet_dict[lane_id]["previous_lane_id_list"]
+  
+  def get_left_lane_id(self, lane_id):
+    return self.lanelet_dict[lane_id]["left_lane_id"]
+
+  def get_right_lane_id(self, lane_id):
+    return self.lanelet_dict[lane_id]["right_lane_id"]
+  
+  def get_waypoint(self, lane_id):
+    return self.lanelet_dict[lane_id]["waypoint"]
+  
+  def get_from_node_id(self, lane_id):
+    return self.lanelet_dict[lane_id]["from_node_id"]
+  
+  def get_to_node_id(self, lane_id):
+    return self.lanelet_dict[lane_id]["to_node_id"]
+  
+  def get_edge_id(self, lane_id):
+    return self.lanelet_dict[lane_id]["edge_id"]
+  
+  def get_lane_id_list_in_edge(self, edge_id):
+    return [lane_id for lane_id in lanelet_dict if lanelet_dict[lane_id]["edge_id"] == edge_id]
