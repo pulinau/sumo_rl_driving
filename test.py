c79ebf580c076e6bbd969678f821bc86da2b5e2a
Changjian Li <c433li@gmail.com>
2018-01-31 20:40:34 -0500

    add some temporary auxiliary code

---
+++ test.py
@@ -0,0 +1,65 @@
+#!python3
+from sumo_gym_config import *
+from action import *
+from observation import *
+from sumo_gym import *
+
+# --------------------------
+#          SUMO
+# --------------------------
+SUMO_TOOLS_DIR = "/home/ken/project/sumo-bin/tools"
+SUMO_BIN = "/home/ken/project/sumo-bin/bin/sumo-gui"
+SUMO_CONFIG = "/home/ken/project/sumo-rl/sumo_openai_gym/traffic/test.sumocfg"
+SUMO_TIME_STEP = 0.1
+SUMO_CMD = [SUMO_BIN, "-c", SUMO_CONFIG, 
+            "--time-to-teleport", "-1", 
+            "--collision.action", "warn", 
+            "--collision.check-junctions", "true", 
+            "--step-length", str(SUMO_TIME_STEP)]
+#            "--lanechange.duration", "2"]
+NET_XML_FILE = "/home/ken/project/sumo-rl/sumo_openai_gym/traffic/test.net.xml"
+
+EGO_VEH_ID = "ego"
+MAX_VEH_ACCEL = 20
+MAX_VEH_DECEL = 20
+MAX_VEH_SPEED = 120
+
+# --------------------------
+#        observation
+# --------------------------
+NUM_LANE_CONSIDERED = 3 # number of lanes considered on each side of ego
+NUM_VEHICLE_CONSIDERED = 16
+OBSERVATION_RADIUS = 600
+
+# --------------------------
+#         reward
+# --------------------------
+MAX_COMFORT_ACCEL = 10
+MAX_COMFORT_DECEL = 10
+
+config = SumoGymConfig(
+               # sumo
+               SUMO_CMD, 
+               SUMO_TIME_STEP, 
+               NET_XML_FILE, 
+               EGO_VEH_ID, 
+               MAX_VEH_ACCEL, 
+               MAX_VEH_DECEL, 
+               MAX_VEH_SPEED, 
+               # observation
+               NUM_LANE_CONSIDERED, 
+               NUM_VEHICLE_CONSIDERED, 
+               OBSERVATION_RADIUS, 
+               # reward
+               MAX_COMFORT_ACCEL, 
+               MAX_COMFORT_DECEL)
+
+
+env = MultiObjSumoEnv(config)
+env.reset()
+
+get_obs_dict(env)
+obs_list, reward_list, done, info = env.step({"lane_change":ActionLaneChange.NOOP, "accel_level":ActionAccel.NOOP})
+print(obs_list)
+
+traci.close()
---
+++ traffic/generate.py
@@ -0,0 +1,38 @@
+#!python3
+import subprocess
+import random
+import os
+
+randomTrip = "~/project/sumo-bin/tools/randomTrips.py"
+#mean = "4"
+#" --binomial=" + mean + \
+
+for i in range(1):
+    period = random.randrange(2, 6)/1000
+    temp0_file_name = "temp0.rou.xml"
+    randomTripArg = " -n test.net.xml -r " + temp0_file_name + \
+                    " -e 0.1" + \
+                    " -p " + str(period) + \
+                    ' --fringe-factor 10 --trip-attributes="departLane=\\"random\\" departSpeed=\\"random\\" departPos=\\"random_free\\""'   
+    subprocess.run(randomTrip + randomTripArg, shell=True, check=True)
+    
+    os.remove("trips.trips.xml")
+
+    period += 0.01
+    temp1_file_name = "temp1.rou.xml"
+    randomTripArg = " -n test.net.xml -r " + temp1_file_name + \
+                    ' --prefix 1_' + \
+                    " -b 0.2 -e 640" + \
+                    " -p " + str(period) + \
+                    ' --fringe-factor 10 --trip-attributes="departLane=\\"random\\" departSpeed=\\"random\\" departPos=\\"base\\""'
+    subprocess.run(randomTrip + randomTripArg, shell=True, check=True)
+    
+    with open(temp0_file_name, "r+") as fd_in0, open(temp1_file_name, "r+") as fd_in1, open("test"+str(i)+".rou.xml", "w+") as fd_out:
+        contents0 = fd_in0.readlines()
+        contents1 = fd_in1.readlines()
+
+        contents = contents0[:-1] + contents1[33:]
+        
+        contents.insert(33, '    <vType id="EgoCar" color="0,1,0"/>\n')
+        contents[34] = '    <vehicle id="ego" depart="0.00" departLane="random" departPos="random_free" departSpeed="random" type="EgoCar">\n'
+        fd_out.writelines(contents)
