fix ttc calculation (considered the size of vehicle)

dqn.py
@@ -168,10 +168,9 @@ class DQNAgent:
       # print(self.name, " empty")
       return
 
-    for model, target_model, loss_hist in zip(self.model_list + [self.model],
-                                              self.target_model_list + [self.target_model],
-                                              self.loss_hist_list + [self.loss_hist]):
-
+    for model_index, (model, target_model, loss_hist) in enumerate(zip(self.model_list + [self.model],
+                                                                       self.target_model_list + [self.target_model],
+                                                                       self.loss_hist_list + [self.loss_hist])):
       rewards = np.array(rewards)
       not_dones = np.array(not_dones)
 
@@ -204,11 +203,14 @@ class DQNAgent:
 
       loss = model.train_on_batch(states, targets_f)
       if self.name == "safety":
-        print(loss[0])
+        print("model ", model_index, " ", loss[0])
 
       loss_hist.append(loss[0])
+
       ep = 0
-      while loss[0] > 1 * np.median(loss_hist) and ep < 10:
+      factor = 2
+      max_train_ep = loss[0]/np.median(loss_hist) - factor
+      while loss[0] > factor * np.median(loss_hist) and ep < 100 * max_train_ep and model_index == len(self.model_list):
         ep += 1
         targets_f = model.predict_on_batch(states)
         # clamp incorrect target to zero
@@ -220,7 +222,7 @@ class DQNAgent:
             x[actions[i][j]] = targets[i][j]
         loss = model.train_on_batch(states, targets_f)
         if self.name == "safety":
-          print(self.name, "supplementary training:", np.median(loss_hist), loss[0])
+          print("model ", model_index, " ", self.name, " supplementary training:", np.median(loss_hist), loss[0])
 
     self.reset_models()
     if self.gamma < self.gamma_max:

dqn_cfgs.py
@@ -147,7 +147,7 @@ def build_model_safety():
   y = tf.keras.layers.minimum(veh_y)
 
   model = tf.keras.models.Model(inputs=[ego_input] + veh_inputs, outputs=veh_y + [y])
-  opt = tf.keras.optimizers.RMSprop(lr=0.001)
+  opt = tf.keras.optimizers.RMSprop(lr=0.0001)
   model.compile(loss='logcosh', optimizer=opt)
 
   return model
@@ -353,7 +353,7 @@ cfg_safety = DQNCfg(name = "safety",
                     threshold = -0.05,
                     memory_size = 3200,
                     traj_end_pred = returnTrue(),
-                    replay_batch_size = 320,
+                    replay_batch_size = 32,
                     traj_end_ratio= 0.0001,
                     _build_model = build_model_safety,
                     model_rst_prob_list = [1/5000, 1/20000, 1/100000, 1/1000000],

reward.py
@@ -23,8 +23,12 @@ def get_reward_safety(env):
   for i, c in enumerate(obs_dict["collision"]):
     r = 0
     d = False
-    if (old_obs_dict is not None and old_obs_dict["ttc"][i] > obs_dict["ttc"][i] + 0.000001 and
-        (obs_dict["ttc"][i] < 4 or np.linalg.norm(obs_dict["relative_position"][i]) < 6)
+    if (old_obs_dict is not None and (
+          old_obs_dict["ttc"][i] > obs_dict["ttc"][i] + 0.0000001 or (
+            np.linalg.norm(old_obs_dict["relative_position"][i]) > np.linalg.norm(obs_dict["relative_position"][i]) + 0.001 and
+            np.linalg.norm(obs_dict["relative_position"][i]) <  4)
+          ) and
+        (obs_dict["ttc"][i] < 4 or np.linalg.norm(obs_dict["relative_position"][i]) < 8)
         ) or (env.env_state == EnvState.CRASH and c == 1
         ) or (action_dict["lane_change"] != ActionAccel.NOOP and obs_dict["ttc"][i] < 4):
       r = -1
@@ -46,7 +50,7 @@ def get_reward_regulation(env):
 
   if obs_dict["ego_dist_to_end_of_lane"] < 100:
     if obs_dict["ego_correct_lane_gap"] != 0:
-      r = -0.1
+      r = 1/(1 + np.exp(-0.1*(obs_dict["ego_dist_to_end_of_lane"]-20))) - 1
 
   if obs_dict["ego_dist_to_end_of_lane"] < 3 and \
      obs_dict["ego_has_priority"] != 1 and \